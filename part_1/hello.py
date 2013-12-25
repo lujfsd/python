cf7acc66e62debb33a14ea08b25a85509ae69009
lujiefeng <lujfsd@163.com>
2013-12-25 17:24:44 +0800

    python的第一个程序hello

---
+++ part_1/hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding=utf-8
+#########################################
+# File Name: hello.py
+# Author: Lu
+# Created Time: 2013年12月25日 星期三 17时08分45秒
+#########################################
+print 'This is Hello World!'
+
+#python 中变量不需要声明就可赋值使用
+#type()函数，查询变量类型
+#print 可以打印多个输出，以逗号隔开
+a = 10
+print a,type(a)
+
+a = 1.3
+print a,type(a)
+
+a = 'Hello World!'
+print a,type(a)
+
+a = True
+print a,type(a)
